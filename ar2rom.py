67f7d6cdfbf1fa0bd96394287c0f1cde8c0e97ff
corentinnormant <corentin.normant@gmail.com>
2016-10-12 12:16:46 +0200

    Added 50 Change in uppercase
    Fix special case for 4
    
    Add entry in the conversion map

--- ar2rom.py
+++ ar2rom.py
@@ -4,7 +4,17 @@ conversion_map = (
     ('x', 10),
     ('ix', 9),
     ('v', 5),
+    ('iv', 4),
     ('i', 1))
+
+
+conversion_map = (
+    ('L', 50),
+    ('X', 10),
+    ('IX', 9),
+    ('V', 5),
+    ('IV', 4),
+    ('I', 1))
 
 
 def ar2rom(val):
@@ -18,31 +28,33 @@ def ar2rom(val):
 
 class Tests(unittest.TestCase):
     def test_1_to_i(self):
-        self.assertEqual(ar2rom(1), 'i')
+        self.assertEqual(ar2rom(1), 'I')
 
     def test_2_to_ii(self):
-        self.assertEqual(ar2rom(2), 'ii')
+        self.assertEqual(ar2rom(2), 'II')
 
     def test_3_to_iii(self):
-        self.assertEqual(ar2rom(3), 'iii')
+        self.assertEqual(ar2rom(3), 'III')
 
     def test_4_to_iv(self):
-        self.assertEqual(ar2rom(4), 'iv')
+        self.assertEqual(ar2rom(4), 'IV')
 
     def test_5_to_v(self):
-        self.assertEqual(ar2rom(5), 'v')
+        self.assertEqual(ar2rom(5), 'V')
 
     def test_9_to_ix(self):
         self.assertEqual(ar2rom(9), 'ix')
 
     def test_10_to_x(self):
-        self.assertEqual(ar2rom(10), 'x')
+        self.assertEqual(ar2rom(10), 'X')
 
     def test_20_to_xx(self):
-        self.assertEqual(ar2rom(20), 'xx')
+        self.assertEqual(ar2rom(20), 'XX')
 
     def test_21_to_xxi(self):
-        self.assertEqual(ar2rom(21), 'xxi')
+        self.assertEqual(ar2rom(21), 'XXI')
+    def test_50_to_l(self):
+        self.assertEqual(ar2rom(50), 'L')    
 
 if __name__ == "__main__":
     unittest.main()
